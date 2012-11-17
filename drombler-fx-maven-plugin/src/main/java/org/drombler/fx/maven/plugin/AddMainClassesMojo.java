beea3e1067b75c467afc915066350a2e1d4c1a97
puce <puce@users.sourceforge.net>
2012-11-17 14:41:48 +0100

    - License header

--- drombler-fx-maven-plugin/src/main/java/org/drombler/fx/maven/plugin/AddMainClassesMojo.java
+++ drombler-fx-maven-plugin/src/main/java/org/drombler/fx/maven/plugin/AddMainClassesMojo.java
@@ -1,6 +1,16 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce).
+ * Copyright 2012 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
  */
 package org.drombler.fx.maven.plugin;
 
