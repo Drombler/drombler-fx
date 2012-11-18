ae068b7c0148cd1b8e4496049f8372c2f0017d72
puce <puce@users.sourceforge.net>
2012-11-18 22:22:22 +0100

    - Sample extended

---
+++ drombler-fx-maven-archetype-application/archetype-resources/__rootArtifactId__-sample/src/main/java/sample/ColoredCircle.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ${package}.sample.impl;
+
+/**
+ *
+ * @author puce
+ */
+public enum ColoredCircle {
+    RED,
+    YELLOW,
+    BLUE;
+}
