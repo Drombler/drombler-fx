e968cf85c8b9487cd2e29b70935112187bbaccc0
puce <puce@users.sourceforge.net>
2014-04-27 16:08:09 +0200

    [#33] Release Javadoc online - Package descriptions

--- drombler-fx-maven-plugin/src/main/java/org/drombler/fx/maven/plugin/package-info.java
+++ drombler-fx-maven-plugin/src/main/java/org/drombler/fx/maven/plugin/package-info.java
@@ -8,11 +8,11 @@
  *
  * The Original Code is Drombler.org. The Initial Developer of the
  * Original Code is Florian Brunner (Sourceforge.net user: puce).
- * Copyright 2012 Drombler.org. All Rights Reserved.
+ * Copyright 2014 Drombler.org. All Rights Reserved.
  *
  * Contributor(s): .
  */
 /**
- * The main classes to startup a Drombler FX based application.
+ * The Drombler FX Application Maven Plugin Mojos.
  */
-package org.drombler.fx.startup.main;
+package org.drombler.fx.maven.plugin;
