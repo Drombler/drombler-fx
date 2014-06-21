07ba299fe98b784cf8780fb6f5587c4f629ce474
puce <puce@users.sourceforge.net>
2014-06-21 02:13:08 +0200

    [#55] Provide repeating annotations - Menu - ToolBar

--- drombler-fx-maven-archetype-application/src/main/resources/archetype-resources/__rootArtifactId__-sample/src/main/java/sample/impl/package-info.java
+++ drombler-fx-maven-archetype-application/src/main/resources/archetype-resources/__rootArtifactId__-sample/src/main/java/sample/impl/package-info.java
@@ -7,17 +7,11 @@
  * and open the template in the editor.
  */
 
-@Menus({
-    @Menu(id = "Custom", displayName = "%customMenu.displayName", position = 110),
-    @Menu(id = "Sub", displayName = "%subMenu.displayName", path = "Custom", position = 30)
-})
-@ToolBars({
-    @ToolBar(id = "rectangle", displayName = "%rectangleToolBar.displayName", position = 50),
-    @ToolBar(id = "circle", displayName = "%circleToolBar.displayName", position = 100)
-})
+@Menu(id = "Custom", displayName = "%customMenu.displayName", position = 110)
+@Menu(id = "Sub", displayName = "%subMenu.displayName", path = "Custom", position = 30)
+@ToolBar(id = "rectangle", displayName = "%rectangleToolBar.displayName", position = 50)
+@ToolBar(id = "circle", displayName = "%circleToolBar.displayName", position = 100)
 package ${package}.sample.impl;
 
 import org.drombler.acp.core.action.Menu;
-import org.drombler.acp.core.action.Menus;
 import org.drombler.acp.core.action.ToolBar;
-import org.drombler.acp.core.action.ToolBars;
