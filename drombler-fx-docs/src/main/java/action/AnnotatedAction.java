18ad65e5557b11a8ce17e492a5eb9964113ef91d
puce <puce@users.sourceforge.net>
2015-04-17 01:44:22 +0200

    [#74] Migrate the Wiki documentation to AsciiDoc - actions, menus and toolbars

---
+++ drombler-fx-docs/src/main/java/action/AnnotatedAction.java
@@ -0,0 +1,23 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce).
+ * Copyright 2015 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package action;
+
+import org.drombler.acp.core.action.Action;
+
+@Action(id = "test1", category = "test", displayName = "%test1.displayName",
+        accelerator = "Shortcut+T", icon = "test1.png")
+public class AnnotatedAction {
+
+}
