9be1a3c018f155a3ae64742b362c516ede2fa7fd
Florian Brunner <fbrunnerreg@gmx.ch>
2016-03-15 22:46:25 +0100

    #126 own-layout-error -Remove unnecessary applicationExecutor

--- drombler-fx-core-standard-desktop-classic/src/main/java/org/drombler/fx/core/standard/desktop/classic/impl/ClassicDesktopRootNodeProvider.java
+++ drombler-fx-core-standard-desktop-classic/src/main/java/org/drombler/fx/core/standard/desktop/classic/impl/ClassicDesktopRootNodeProvider.java
@@ -1,16 +1,13 @@
 package org.drombler.fx.core.standard.desktop.classic.impl;
 
 import java.io.IOException;
-import java.util.concurrent.Executor;
 import javafx.scene.Parent;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
-import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.drombler.acp.core.action.spi.ApplicationToolBarContainerProvider;
 import org.drombler.acp.core.action.spi.MenuBarMenuContainerProvider;
-import org.drombler.acp.startup.main.ApplicationExecutorProvider;
 import org.drombler.fx.startup.main.MainSceneRootProvider;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.ComponentContext;
@@ -27,34 +24,21 @@ public class ClassicDesktopRootNodeProvider implements MainSceneRootProvider {
 
     private static final Logger LOG = LoggerFactory.getLogger(ClassicDesktopRootNodeProvider.class);
 
-    @Reference
-    private ApplicationExecutorProvider applicationExecutorProvider;
-
     private ClassicDesktopApplicationPane root;
-
-    protected void bindApplicationExecutorProvider(ApplicationExecutorProvider applicationExecutorProvider) {
-        this.applicationExecutorProvider = applicationExecutorProvider;
-    }
-
-    protected void unbindApplicationExecutorProvider(ApplicationExecutorProvider applicationExecutorProvider) {
-        this.applicationExecutorProvider = null;
-    }
 
     @Activate
     protected void activate(ComponentContext context) {
-        getApplicationExecutor().execute(() -> {
-            try {
-                root = new ClassicDesktopApplicationPane();
-                context.getBundleContext().registerService(
-                        new String[]{
-                            MenuBarMenuContainerProvider.class.getName(),
-                            ContentPaneProvider.class.getName(),
-                            ApplicationToolBarContainerProvider.class.getName()
-                        }, root, null);
-            } catch (IOException ex) {
-                LOG.error(ex.getMessage(), ex);
-            }
-        });
+        try {
+            root = new ClassicDesktopApplicationPane();
+            context.getBundleContext().registerService(
+                    new String[]{
+                        MenuBarMenuContainerProvider.class.getName(),
+                        ContentPaneProvider.class.getName(),
+                        ApplicationToolBarContainerProvider.class.getName()
+                    }, root, null);
+        } catch (IOException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
     }
 
     @Deactivate
@@ -73,10 +57,6 @@ public class ClassicDesktopRootNodeProvider implements MainSceneRootProvider {
     @Override
     public Parent getRoot() {
         return root;
-    }
-
-    private Executor getApplicationExecutor() {
-        return applicationExecutorProvider.getApplicationExecutor();
     }
 
 }
