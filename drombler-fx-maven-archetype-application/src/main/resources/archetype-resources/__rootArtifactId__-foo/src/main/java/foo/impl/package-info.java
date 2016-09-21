b0fd67d8c0cd6cc8b28eaeb992e846c225f1f74c
Florian Brunner <fbrunnerreg@gmx.ch>
2016-09-22 01:40:07 +0200

    #111 File Type Editor Registration - sample: foo file support added

---
+++ drombler-fx-maven-archetype-application/src/main/resources/archetype-resources/__rootArtifactId__-foo/src/main/java/foo/impl/package-info.java
@@ -0,0 +1,8 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+
+@FileExtension(displayName = "%fooFileExtension.displayName", mimeType = FooUtils.MIME_TYPE, fileExtensions = FooUtils.FILE_EXTENSION, position = 10)
+package ${package}.foo.impl;
+
+import org.drombler.acp.core.data.FileExtension;
