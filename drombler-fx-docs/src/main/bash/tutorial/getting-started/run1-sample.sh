2deb6775be365f7a0547f7eda3fdbc1c12957c9b
Florian Brunner <fbrunnerreg@gmx.ch>
2016-10-20 00:40:50 +0200

    #111 File Type Editor Registration - tutorial

--- drombler-fx-docs/src/main/bash/tutorial/getting-started/run1-sample.sh
+++ drombler-fx-docs/src/main/bash/tutorial/getting-started/run1-sample.sh
@@ -1,3 +1,3 @@
 #!/bin/bash          
 cd test-application
-java -Djavafx.verbose=true -Dbinary.css=false -Djava.util.logging.config.file=target/deployment/standalone/conf/logging.properties -jar target/deployment/standalone/bin/foo.jar --userdir target/userdir
+java -Duser.language=en -Duser.country=US -Djavafx.verbose=true -Dbinary.css=false -Djava.util.logging.config.file=target/deployment/standalone/conf/logging.properties -jar target/deployment/standalone/bin/foo.jar --userdir target/userdir
