a4f0c337754cac1bca5559eed68dfb3058a609b5
Florian Brunner <fbrunnerreg@gmx.ch>
2017-01-14 20:20:35 +0100

    #137 Provide packaging support - Maven Plugin: set-maven-properties Mojo added - Lifecycle enhancements

--- drombler-fx-maven-plugin/src/main/java/org/drombler/fx/maven/plugin/SetMavenProperties.java
+++ drombler-fx-maven-plugin/src/main/java/org/drombler/fx/maven/plugin/SetMavenProperties.java
@@ -19,11 +19,13 @@ import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
-//@Mojo(name = "set-maven-properties", defaultPhase = LifecyclePhase.INITIALIZE)
+@Mojo(name = "set-maven-properties", defaultPhase = LifecyclePhase.INITIALIZE)
 public class SetMavenProperties extends AbstractMojo {
 
     /**
