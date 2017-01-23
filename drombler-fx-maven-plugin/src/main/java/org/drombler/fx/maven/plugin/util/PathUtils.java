5428bedbec45452ee36eef8dfdc0b89f54d40009
Florian Brunner <fbrunnerreg@gmx.ch>
2017-01-23 22:26:15 +0100

    #137 Provide packaging support - create bin/lib dir

--- drombler-fx-maven-plugin/src/main/java/org/drombler/fx/maven/plugin/util/PathUtils.java
+++ drombler-fx-maven-plugin/src/main/java/org/drombler/fx/maven/plugin/util/PathUtils.java
@@ -8,6 +8,7 @@ public class PathUtils {
 
     public static final String BIN_DIR_NAME = "bin";
     public static final String BUNDLE_DIR_NAME = "bundle";
+    public static final String LIB_DIR_NAME = "lib";
 
     public static String getMainJarName(String brandingId) {
         return brandingId + ".jar";
