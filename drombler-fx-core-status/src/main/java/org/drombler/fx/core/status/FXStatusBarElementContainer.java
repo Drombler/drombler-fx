182feb8caef60b6236d3560edc7ed161b119536a
Florian Brunner <fbrunnerreg@gmx.ch>
2017-06-12 01:34:23 +0200

    #169 Provide a modular status bar - add positionable

--- drombler-fx-core-status/src/main/java/org/drombler/fx/core/status/FXStatusBarElementContainer.java
+++ drombler-fx-core-status/src/main/java/org/drombler/fx/core/status/FXStatusBarElementContainer.java
@@ -1,16 +1,29 @@
 package org.drombler.fx.core.status;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
 import javafx.scene.Node;
+import javafx.scene.control.Separator;
 import org.drombler.acp.core.status.spi.StatusBarElementContainer;
 import org.drombler.commons.fx.scene.control.StatusBar;
 import org.softsmithy.lib.util.PositionableAdapter;
+import org.softsmithy.lib.util.Positionables;
 
 /**
  *
  * @author puce
  */
 public class FXStatusBarElementContainer implements StatusBarElementContainer {
+
+    private static final int SEPARATOR_STEPS = 1000;
+
+    private final List<PositionableAdapter<? extends Node>> leftStatusBarElement = Collections.synchronizedList(new ArrayList<>());// TODO: synchronized needed?
+    private final List<PositionableAdapter<? extends Node>> centerStatusBarElement = Collections.synchronizedList(new ArrayList<>());// TODO: synchronized needed?
+    private final List<PositionableAdapter<? extends Node>> statusStatusBarElement = Collections.synchronizedList(new ArrayList<>());// TODO: synchronized needed?
 
     private final StatusBar statusBar;
 
@@ -20,21 +33,50 @@ public class FXStatusBarElementContainer implements StatusBarElementContainer {
 
     @Override
     public void addLeftStatusBarElement(PositionableAdapter<? extends Node> statusBarElement) {
-        addStatusBarElement(statusBar.getLeftEntries(), statusBarElement);
+        addStatusBarElement(statusBar.getLeftEntries(), leftStatusBarElement, statusBarElement);
     }
 
     @Override
     public void addCenterStatusBarElement(PositionableAdapter<? extends Node> statusBarElement) {
-        addStatusBarElement(statusBar.getCenterEntries(), statusBarElement);
+        addStatusBarElement(statusBar.getCenterEntries(), centerStatusBarElement, statusBarElement);
     }
 
     @Override
     public void addRightStatusBarElement(PositionableAdapter<? extends Node> statusBarElement) {
-        addStatusBarElement(statusBar.getRightEntries(), statusBarElement);
+        addStatusBarElement(statusBar.getRightEntries(), statusStatusBarElement, statusBarElement);
     }
 
-    private void addStatusBarElement(ObservableList<Node> entries, PositionableAdapter<? extends Node> statusBarElement) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void addStatusBarElement(ObservableList<Node> entries, List<PositionableAdapter<? extends Node>> elements, PositionableAdapter<? extends Node> statusBarElementAdapter) {
+        int insertionPoint = Positionables.getInsertionPoint(elements, statusBarElementAdapter);
+        entries.add(insertionPoint, statusBarElementAdapter.getAdapted());
+        elements.add(insertionPoint, statusBarElementAdapter);
+
+        Optional<Integer> separatorInsertionPoint = getSeparatorInsertionPoint(insertionPoint, entries, elements, statusBarElementAdapter);
+
+        if (separatorInsertionPoint != null && separatorInsertionPoint.isPresent()) {
+            entries.add(separatorInsertionPoint.get(), new Separator(Orientation.VERTICAL));
+        }
+
+    }
+
+    private Optional<Integer> getSeparatorInsertionPoint(int insertionPoint, ObservableList<Node> entries, List<PositionableAdapter<? extends Node>> elements,
+            PositionableAdapter<? extends Node> statusBarElementAdapter) {
+        if (insertionPoint < elements.size() - 1
+                && ((elements.get(insertionPoint + 1).getPosition() / SEPARATOR_STEPS) - (statusBarElementAdapter.getPosition() / SEPARATOR_STEPS)) >= 1
+                && !(entries.get(insertionPoint + 1) instanceof Separator)) {
+            return Optional.of(insertionPoint + 1);
+
+        }
+
+        if (insertionPoint > 0
+                && ((statusBarElementAdapter.getPosition() / SEPARATOR_STEPS) - (elements.get(insertionPoint - 1).getPosition() / SEPARATOR_STEPS)) >= 1
+                && !(entries.get(insertionPoint + 1) instanceof Separator)) {
+            return Optional.of(insertionPoint + 1);
+
+        }
+
+        return Optional.empty();
+
     }
 
 }
