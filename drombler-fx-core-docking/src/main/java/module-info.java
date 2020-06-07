427b3a13dc7c01d675c315035146113b3760ae99
Florian Brunner <fbrunnerreg@gmx.ch>
2020-06-07 12:33:33 +0200

    #172 Java 11  - clean-up

--- drombler-fx-core-docking/src/main/java/module-info.java
+++ drombler-fx-core-docking/src/main/java/module-info.java
@@ -2,12 +2,12 @@ module org.drombler.fx.core.docking {
     exports org.drombler.fx.core.docking;
 
     requires org.drombler.fx.core.application;
-    requires org.drombler.acp.core.data;
+    requires transitive org.drombler.acp.core.data;
     requires org.drombler.acp.core.docking.spi;
     requires org.drombler.acp.core.context;
-    requires org.drombler.commons.action.command;
+    requires transitive org.drombler.commons.action.command;
     requires org.drombler.commons.client.core;
-    requires org.drombler.commons.docking.fx;
+    requires transitive org.drombler.commons.docking.fx;
     requires org.drombler.commons.docking.context;
     requires org.drombler.commons.docking.fx.context;
     requires org.apache.felix.framework;
